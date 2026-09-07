7847444b6f65b0780a041cd5562e8d3865c67b0b
Mike Tolhurst <miketolhurst@users.noreply.github.com>
2026-09-07 10:27:26 +1000

    build: configure static export with basePath for projects.tolhurst.me

--- theme-creator-web/next.config.ts
+++ theme-creator-web/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/theme-creator",
 };
 
 export default nextConfig;
